a2b6ea13401ed40df925e16bf44554880b42ad57
Desire Chukwudi <chukwudidesire01@gmail.com>
2023-09-08 20:32:51 +0100

    just a Date fix

--- Controllers/request-controller.js
+++ Controllers/request-controller.js
@@ -1,26 +1,25 @@
-import myData from "../data.js";
+import { generateData } from "../data.js";
 
 const dataRequest = async (req, res) => {
-  //I will use these query parameters to filter my Array of Data and the matching object
   const { slack_name, track } = req.query;
 
   try {
-    //Filtering my Data to find the record that matches the request parameters:
-    const requiredUser = await myData.filter(
-      (data) => data.slack_name === slack_name && data.track === track
+    const data = generateData(); // Get the data array from the module
+    const requiredUser = data.filter(
+      (userData) =>
+        userData.slack_name === slack_name && userData.track === track
     );
-    //When I find a matching Record in my Data, I will add the status_code, and return the Record:
+
     if (requiredUser.length > 0) {
       let myUser = requiredUser[0];
-      console.log("myUser");
       myUser.status_code = 200;
-      console.log(requiredUser);
-      res.status(200).send(myUser);
+      res.status(200).json(myUser);
     } else {
-      return res.send("Invalid Parameters");
+      res.status(400).json({ error: "Invalid Parameters" });
     }
   } catch (err) {
-    throw new Error(err);
+    console.error(err);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 };
 
--- data.js
+++ data.js
@@ -8,67 +8,69 @@ const days = [
   "Saturday",
 ];
 
-let date = new Date();
+const generateData = () => {
+  const date = new Date();
+  const currentDay = days[date.getDay()];
 
-//Getting the day of the week
-let currentDay = days[date.getDay()];
+  const myUtcTime = () => {
+    // Get the date and time components
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, "0"); // Month is zero-based, so add 1
+    const day = String(date.getDate()).padStart(2, "0");
+    const hours = String(date.getHours()).padStart(2, "0");
+    const minutes = String(date.getMinutes()).padStart(2, "0");
+    const seconds = String(date.getSeconds()).padStart(2, "0");
 
-//Getting the Current Time:
+    // Create the ISO 8601 formatted string
+    const isoString = `${year}-${month}-${day}T${hours}:${minutes}:${seconds}Z`;
 
-// Get the date and time components
-let year = date.getFullYear();
-let month = String(date.getMonth() + 1).padStart(2, "0"); // Month is zero-based, so add 1
-let day = String(date.getDate()).padStart(2, "0");
-let hours = String(date.getHours()).padStart(2, "0");
-let minutes = String(date.getMinutes()).padStart(2, "0");
-let seconds = String(date.getSeconds()).padStart(2, "0");
+    return isoString;
+  };
 
-// Create the ISO 8601 formatted string
-const isoString = `${year}-${month}-${day}T${hours}:${minutes}:${seconds}Z`;
+  const data = [
+    {
+      slack_name: "Mr. Dee",
+      current_day: currentDay,
+      utc_time: myUtcTime(),
+      track: "backend",
+      github_file_url:
+        "https://github.com/MrChukwudi/HNGX-Level1/blob/master/index.js",
+      github_repo_url: "https://github.com/MrChukwudi/HNGX-Level1",
+      status_code: "",
+    },
+    {
+      slack_name: "Oladokun",
+      current_day: currentDay,
+      utc_time: myUtcTime(),
+      track: "frontend",
+      github_file_url:
+        "https://github.com/MrChukwudi/HNGX-Level1/blob/master/index.js",
+      github_repo_url: "https://github.com/MrChukwudi/HNGX-Level1",
+      status_code: "",
+    },
+    {
+      slack_name: "Musa",
+      current_day: currentDay,
+      utc_time: myUtcTime(),
+      track: "Frontend",
+      github_file_url:
+        "https://github.com/MrChukwudi/HNGX-Level1/blob/master/index.js",
+      github_repo_url: "https://github.com/MrChukwudi/HNGX-Level1",
+      status_code: "",
+    },
+    {
+      slack_name: "Musa",
+      current_day: currentDay,
+      utc_time: myUtcTime(),
+      track: "backend",
+      github_file_url:
+        "https://github.com/MrChukwudi/HNGX-Level1/blob/master/index.js",
+      github_repo_url: "https://github.com/MrChukwudi/HNGX-Level1",
+      status_code: "",
+    },
+  ];
 
-console.log(isoString);
+  return data;
+};
 
-const data = [
-  {
-    slack_name: "Mr. Dee",
-    current_day: currentDay,
-    utc_time: isoString,
-    track: "backend",
-    github_file_url:
-      "https://github.com/MrChukwudi/HNGX-Level1/blob/master/index.js",
-    github_repo_url: "https://github.com/MrChukwudi/HNGX-Level1",
-    status_code: "",
-  },
-  {
-    slack_name: "Oladokun",
-    current_day: currentDay,
-    utc_time: isoString,
-    track: "frontend",
-    github_file_url:
-      "https://github.com/MrChukwudi/HNGX-Level1/blob/master/index.js",
-    github_repo_url: "https://github.com/MrChukwudi/HNGX-Level1",
-    status_code: "",
-  },
-  {
-    slack_name: "Musa",
-    current_day: currentDay,
-    utc_time: isoString,
-    track: "Frontend",
-    github_file_url:
-      "https://github.com/MrChukwudi/HNGX-Level1/blob/master/index.js",
-    github_repo_url: "https://github.com/MrChukwudi/HNGX-Level1",
-    status_code: "",
-  },
-  {
-    slack_name: "Musa",
-    current_day: currentDay,
-    utc_time: isoString,
-    track: "backend",
-    github_file_url:
-      "https://github.com/MrChukwudi/HNGX-Level1/blob/master/index.js",
-    github_repo_url: "https://github.com/MrChukwudi/HNGX-Level1",
-    status_code: "",
-  },
-];
-
-export default data;
+export { generateData };
